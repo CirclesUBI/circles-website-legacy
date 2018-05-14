eac1b537470269d5f275470fbd8a0a7b62d7da7c
ed murphy <edzillion@gmail.com>
2018-05-14 12:02:12 +0200

    preview image metatags for goog, twit and FB

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var header = require('gulp-header')
+var cleanCSS = require('gulp-clean-css')
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
+var pkg = require('./package.json')
+var browserSync = require('browser-sync').create()
 
-const buildDir = './build';
+const buildDir = './build'
 
 // Set the banner content
 var banner = ['/*!\n',
@@ -16,46 +16,44 @@ var banner = ['/*!\n',
   ' * Licensed under <%= pkg.license %> (https://github.com/CirclesUBI/cafe-grundeinkommen-website/blob/master/LICENSE)\n',
   ' */\n',
   ''
-].join('');
+].join('')
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
-
+gulp.task('vendor', function () {
   // Bootstrap
   gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
+    './node_modules/bootstrap/dist/**/*',
+    '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+    '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+  ])
     .pipe(gulp.dest(buildDir + '/vendor/bootstrap'))
 
   // Font Awesome
   gulp.src([
-      './node_modules/font-awesome/**/*',
-      '!./node_modules/font-awesome/{less,less/*}',
-      '!./node_modules/font-awesome/{scss,scss/*}',
-      '!./node_modules/font-awesome/.*',
-      '!./node_modules/font-awesome/*.{txt,json,md}'
-    ])
+    './node_modules/font-awesome/**/*',
+    '!./node_modules/font-awesome/{less,less/*}',
+    '!./node_modules/font-awesome/{scss,scss/*}',
+    '!./node_modules/font-awesome/.*',
+    '!./node_modules/font-awesome/*.{txt,json,md}'
+  ])
     .pipe(gulp.dest(buildDir + '/vendor/font-awesome'))
 
   // jQuery
   gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
+    './node_modules/jquery/dist/*',
+    '!./node_modules/jquery/dist/core.js'
+  ])
     .pipe(gulp.dest(buildDir + '/vendor/jquery'))
 
   // jQuery Easing
   gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
+    './node_modules/jquery.easing/*.js'
+  ])
     .pipe(gulp.dest(buildDir + '/vendor/jquery-easing'))
-
-});
+})
 
 // Compile SCSS
-gulp.task('css:compile', function() {
+gulp.task('css:compile', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass.sync({
       outputStyle: 'expanded'
@@ -64,31 +62,31 @@ gulp.task('css:compile', function() {
       pkg: pkg
     }))
     .pipe(gulp.dest(buildDir + '/css'))
-});
+})
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+gulp.task('css:minify', ['css:compile'], function () {
   return gulp.src([
-      buildDir + '/css/*.css',
-      '!' + buildDir + '/css/*.min.css'
-    ])
+    buildDir + '/css/*.css',
+    '!' + buildDir + '/css/*.min.css'
+  ])
     .pipe(cleanCSS())
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(gulp.dest(buildDir + '/css'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+gulp.task('css', ['css:compile', 'css:minify'])
 
 // Minify JavaScript
-gulp.task('js:minify', function() {
+gulp.task('js:minify', function () {
   return gulp.src([
-      './js/*.js',
-      '!./js/*.min.js'
-    ])
+    './js/*.js',
+    '!./js/*.min.js'
+  ])
     .pipe(uglify())
     .pipe(rename({
       suffix: '.min'
@@ -97,44 +95,43 @@ gulp.task('js:minify', function() {
       pkg: pkg
     }))
     .pipe(gulp.dest(buildDir + '/js'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 gulp.task('files:main', function () {
   return gulp.src([
-      './img//**/*',
-      './mail//**/*',
-      'index.html'      
-    ], {base: './'}) 
-    .pipe(gulp.dest(buildDir));
-});
+    './img//**/*',
+    './mail//**/*',
+    'index.html'
+  ], {base: './'}).pipe(gulp.dest(buildDir))
+})
 
 gulp.task('files:favicons', function () {
   return gulp.src('./favicons//**/*')
-    .pipe(gulp.dest(buildDir));
-});
+    .pipe(gulp.dest(buildDir))
+})
 
 // Move files to build
-gulp.task('files', ['files:main', 'files:favicons']);
+gulp.task('files', ['files:main', 'files:favicons'])
 
 // JS
-gulp.task('js', ['js:minify']);
+gulp.task('js', ['js:minify'])
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor', 'files']);
+gulp.task('default', ['css', 'js', 'vendor', 'files'])
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
     server: {
       baseDir: buildDir
     }
-  });
-});
+  })
+})
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'vendor', 'files', 'browserSync'], function() {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
-  gulp.watch('./*.html', ['files',browserSync.reload]);
-});
+gulp.task('dev', ['css', 'js', 'vendor', 'files', 'browserSync'], function () {
+  gulp.watch('./scss/*.scss', ['css'])
+  gulp.watch('./js/*.js', ['js'])
+  gulp.watch('./*.html', ['files', browserSync.reload])
+})
